f04829c36c8dce3800b48a7f1eaf0c72d6152900
JohnnyTang <1275441282@qq.com>
2023-11-27 09:38:11 +0800

    finish tzh part(tbd)

--- front/src/views/Index.vue
+++ front/src/views/Index.vue
@@ -2,17 +2,24 @@
   <div class="home">
     <el-container>
       <el-header>
-        <el-row style="height: 100%" justify="center" align="middle">
+        <el-row style="height: 100%" justify="start" align="middle">
+          <el-col :span="9">
+            <RouterLink to="/home"
+              ><span class="title-item" :class="{ active: curIndex === 1 }"
+                >潮汐河段崩岸监测预警系统</span
+              >
+            </RouterLink></el-col
+          >
           <el-col :span="2">
             <RouterLink to="/home"
               ><span class="nav-item" :class="{ active: curIndex === 1 }"
-                >首页</span
+                >崩岸风险预警</span
               >
             </RouterLink></el-col
           >
           <el-col :span="2"
             ><RouterLink to="/statistics"
-              ><span class="nav-item" :class="{ active: curIndex === 2 }">统计分析</span>
+              ><span class="nav-item" :class="{ active: curIndex === 2 }">监测数据统计</span>
             </RouterLink></el-col
           >
           <el-col :span="2"
@@ -21,7 +28,7 @@
                 class="nav-item"
                 style="margin-left: 10px"
                 :class="{ active: curIndex === 3 }"
-                >设备维护</span
+                >远程设备管理</span
               >
             </RouterLink></el-col
           >
@@ -100,9 +107,26 @@ watchEffect(() => {
 span.nav-item {
   color: white;
   text-decoration: none;
-  font-size: calc(0.6vh + 0.8vw);
+  font-size: calc(0.6vh + 0.75vw);
   font-weight: 600;
   font-family: 'SimHei';
+}
+
+span.title-item {
+  color: white;
+  text-decoration: none;
+  font-size: calc(0.6vh + 1vw);
+  font-weight: 600;
+  font-family: 'SimHei';
+
+  letter-spacing: 0.2em;
+
+  background-image: linear-gradient(25deg,
+        #fdfffc 0%,
+        #a7d1ec 40%,
+        #3862eb 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 }
 
 span.nav-item:hover {
